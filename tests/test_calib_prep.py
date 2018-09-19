55a3b6c2f99683676d512560956842dcebbfa8c5
Bryan Hilbert <hilbert@stsci.edu>
2018-09-19 10:33:16 -0400

    All pytests now passing. Bugfix in file_search in calib_prep.py

--- tests/test_calib_prep.py
+++ tests/test_calib_prep.py
@@ -10,35 +10,16 @@ Use
     parent directory of jwst_reffiles/tests/:
     >>> pytest
 '''
+from collections import OrderedDict
+import copy
+import os
 
+from astropy.io import ascii
 from jwst.pipeline import Detector1Pipeline
 
-from jwst_reffiles.pipeline.calib_prep import calibPrep
+import jwst_reffiles
+from jwst_reffiles.pipeline.calib_prep import CalibPrep
 from jwst_reffiles.utils.definitions import PIPE_STEPS
-
-
-def initialize_calib_prep():
-    """Create instance of calibPrep and prepare for later tests
-    """
-    instance = calibPrep()
-    instance.verbose = False
-    instance.inputs =
-    instance.search_dir = ''
-    instance.output_dir = ''
-    return instance
-
-
-def test_pipeline_step_list():
-    """Basic test that the number of steps in CALWEBB_DETECTOR1 is what we expect"""
-    pipeline = Detector1Pipeline()
-    assert len(pipeline.step_defs) == 15
-
-
-def test_calib_prep_steps():
-    """Wrapper around tests of individual functions in calib.prep.py"""
-    obj = initialize_calib_prep()
-    choose_file_test(obj)
-    get_basename_test(obj)
 
 
 def choose_file_test(cp_object):
@@ -50,9 +31,9 @@ def choose_file_test(cp_object):
     files = ['file1.fits', 'file2.fits', 'file3.fits', 'file4.fits', 'file5.fits']
 
     # Create a basic copy of the pipeline step dictionary
-    all_false = copy.deepcopy(PIPE_STEPS)
-    for key in all_false:
-        all_false[key] = False
+    #all_false = copy.deepcopy(OrderedDict(PIPE_STEPS))
+    #for key in all_false:
+    #    all_false[key] = False
 
     # Create a step dictionary for each input file that lists its
     # current calibration state
@@ -71,7 +52,8 @@ def choose_file_test(cp_object):
     required.append(create_step_dictionary('dq, sat, super, ref, ipc, dark, jump'))
     required.append(create_step_dictionary('dq, sat'))
     required.append(create_step_dictionary('dq, sat, ref'))
-    required.append(create_step_dictionary('dq, sat, super, ref, ipc, jump, rate'))
+    required.append(create_step_dictionary('dq, sat, super, ref, ipc, jump, rampfit'))
+    # required.append(create_step_dictionary('dq'))
 
     # Select the best files
     chosen = []
@@ -82,12 +64,24 @@ def choose_file_test(cp_object):
     assert chosen == truth
 
 
-def get_basename_test(cp_object):
-    """Test of simple script to remove fits from filename"""
-    filename = "jw00000999888_test_file_cal.fits"
+def completed_steps_test(cp_object):
+    """Test that header keywords describing the calibration state are
+    correctly translated into a boolean dictionary"""
+    filename = os.path.join(os.getcwd(), 'test_data', 'file_for_completed_steps_test.fits')
+    truth = create_step_dictionary('dq, sat, super, ref, lin')
+    test_case = cp_object.completed_steps(filename)
+    assert truth == test_case
 
-    basename = cp_object.get_file_basename(filename)
-    assert basename == filename.strip('.fits')
+
+def create_output_test(cp_object):
+    """Test that the correct output filename is generated for a given
+    requested calibration state and input file
+    """
+    requested = create_step_dictionary('dq, sat, ref, super, jump')
+    basefilename = 'dark_current_file_number_42_uncal'
+    truth = os.path.join(cp_object.output_dir, basefilename.replace('uncal', 'jump.fits'))
+    constructed_name, true_basename = cp_object.create_output(basefilename, requested)
+    assert constructed_name == truth
 
 
 def create_step_dictionary(steplist):
@@ -104,16 +98,98 @@ def create_step_dictionary(steplist):
     step_dict : dict
         Dictionary with requested steps set to True
     """
-    all_false = copy.deepcopy(PIPE_STEPS)
+    all_false = copy.deepcopy(OrderedDict(PIPE_STEPS))
     for key in all_false:
         all_false[key] = False
 
-    step_dict = {}
-    pipesteps = [PIPE_STEPS[element.strip()] for element in steplist.split(',')]
+    step_dict = copy.deepcopy(all_false)
+    pipesteps = [element.strip() for element in steplist.split(',')]
     for stepname in pipesteps:
-        step_dict[stepname] = True
+        if stepname in all_false.keys():
+            step_dict[stepname] = True
+        else:
+            raise ValueError("{} is not a valid step dictionary entry.".format(stepname))
     return step_dict
 
 
-def test_trivial():
-    assert 1 == 1
+def get_basename_test(cp_object):
+    """Test of simple script to remove fits from filename"""
+    filename = "jw00000999888_test_file_cal.fits"
+    basename = cp_object.get_file_basename(filename)
+    assert basename == filename.strip('.fits')
+
+
+def initialize_calib_prep():
+    """Create instance of calibPrep and prepare for later tests
+    """
+    instance = CalibPrep()
+    instance.verbose = False
+    # instance.inputs = ascii.read('test_calib_input_table.txt')
+    instance.inputs = 'nothing'
+    instance.search_dir = jwst_reffiles.__path__
+    instance.output_dir = '/dummy/output/dir/'
+    return instance
+
+
+def steps_to_run_test(cp_object):
+    """Test that the correct dictionary for calibration steps to be run
+    is generated given an input file in some current state of calibration
+    as well as a list of total calibration steps required.
+    """
+    required = create_step_dictionary('dq, sat, super, ref, dark, jump')
+    current1 = create_step_dictionary('sat, ref, dark')
+    current2 = create_step_dictionary('dq')
+    truth1 = create_step_dictionary('dq, super, jump')
+    truth2 = create_step_dictionary('sat, super, ref, dark, jump')
+    torun1 = cp_object.steps_to_run('dummy.fits', required, current1)
+    torun2 = cp_object.steps_to_run('dummy.fits', required, current2)
+    assert torun1 == truth1
+    assert torun2 == truth2
+
+
+def strun_command_test(cp_object):
+    """Test that the correct strun commands are generated for a given
+    input file and required calibration state
+    """
+    from astropy.table import Column
+
+    in_names = ['dummy.fits', 'dummy.fits', 'dummy.fits']
+    input_names = Column(data=in_names)
+    torun1 = 'sat,super,dark,jump'
+    torun2 = 'dq,super,ref'
+    torun3 = 'dark,lin,rampfit'
+    steps_to_run = Column(data=[torun1, torun2, torun3])
+    out_names = ['dummy_jump.fits', 'dummy_refpix.fits', 'dummy_rate.fits']
+    output_filename = Column(data=out_names)
+    constructed_commands = cp_object.strun_command(input_names, steps_to_run, output_filename)
+    truth1 = ('strun calwebb_detector1.cfg {} --steps.dq_init.skip=True --steps.refpix.skip=True '
+              '--steps.ipc.skip=True --steps.linearity.skip=True --steps.persistence.skip=True '
+              '--steps.ramp_fitting.skip=True --steps.jump.output_file={}'.format(in_names[0], out_names[0]))
+    truth2 = ('strun calwebb_detector1.cfg {} --steps.saturation.skip=True --steps.ipc.skip=True '
+              '--steps.linearity.skip=True --steps.persistence.skip=True --steps.dark_current.skip=True '
+              '--steps.jump.skip=True --steps.ramp_fitting.skip=True --steps.refpix.output_file={}'
+              .format(in_names[1], out_names[1]))
+    truth3 = ('strun calwebb_detector1.cfg {} --steps.dq_init.skip=True --steps.saturation.skip=True '
+              '--steps.superbias.skip=True --steps.refpix.skip=True --steps.ipc.skip=True '
+              '--steps.persistence.skip=True --steps.jump.skip=True --steps.ramp_fitting.output_file={}'
+              .format(in_names[2], out_names[2]))
+    truths = [truth1, truth2, truth3]
+    assert truths == constructed_commands
+
+
+def test_calib_prep_steps():
+    """Wrapper around tests of individual functions in calib.prep.py"""
+    obj = initialize_calib_prep()
+    choose_file_test(obj)
+    get_basename_test(obj)
+    choose_file_test(obj)
+    completed_steps_test(obj)
+    create_output_test(obj)
+    steps_to_run_test(obj)
+    strun_command_test(obj)
+
+
+def test_pipeline_step_list():
+    """Basic test that the number of steps in CALWEBB_DETECTOR1 is what we expect"""
+    pipeline = Detector1Pipeline()
+    assert len(pipeline.step_defs) == 15
