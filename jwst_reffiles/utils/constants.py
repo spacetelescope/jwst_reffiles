6b16727583e7377fd76ac44af2d06970193f5ff2
Bryan Hilbert <hilbert@stsci.edu>
2019-09-25 16:47:23 -0400

    Straighten up filenames of files to be read in.

---
+++ jwst_reffiles/utils/constants.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+"""This file contains constants that may be useful across multiple
+jwst_reffiles modules
+"""
+
+RATE_FILE_SUFFIXES = ['_0_ramp_fit', '_1_ramp_fit', '_ramp_fit_0', '_ramp_fit_1']
